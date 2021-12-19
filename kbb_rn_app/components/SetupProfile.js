ba14a949516281d396284eb46ee1afc7f2efb996
Jeesub Kim <zays.kim@gmail.com>
2021-12-19 15:26:56 +0900

    Adding Image upload feature using firebase store, used base64 encoding especially for android because of permission issue

--- kbb_rn_app/components/SetupProfile.js
+++ kbb_rn_app/components/SetupProfile.js
@@ -1,37 +1,92 @@
-import {useNavigation, useRoute} from '@react-navigation/native';
 import React, {useState} from 'react';
-
-import {View, StyleSheet} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Pressable,
+  Platform,
+  Image,
+  ActivityIndicator,
+} from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import storage from '@react-native-firebase/storage';
 import {signOut} from '../lib/auth/auth';
 import {createUser} from '../lib/db/users';
 import Button from './Button';
 import Input from './Input';
 import useUserContext from '../hooks/useUserContext';
+import {launchImageLibrary} from 'react-native-image-picker';
 
 export default function SetupProfile() {
   const [displayName, setDisplayName] = useState();
+  const [image, setImage] = useState();
+  const [loading, setLoading] = useState(false);
   const navigation = useNavigation();
   const {user, setUser} = useUserContext();
   const {params} = useRoute();
   const {uid} = params || {};
 
-  const onSubmit = () => {
+  const onSubmit = async () => {
+    setLoading(true);
+    let photoURL = null;
+    if (image) {
+      const asset = image.assets[0];
+      const extension = asset.fileName.split('.').pop();
+      const reference = storage().ref(`/profile/${uid}.${extension}`);
+
+      if (Platform.OS === 'android') {
+        //encoded by base64
+        await reference.putString(asset.base64, 'base64', {
+          contentType: asset.type,
+        });
+      } else {
+        await reference.putFile(asset.uri); //upload uri directly with ios
+      }
+
+      photoURL = image ? await reference.getDownloadURL() : null;
+    }
     const userInfo = {
       id: uid,
       displayName,
-      photoURL: null,
+      photoURL,
     };
     createUser(userInfo);
     setUser(userInfo);
+    setLoading(false);
   };
 
   const onCancel = () => {
     signOut();
     navigation.goBack();
   };
+  const onSelectImage = () => {
+    launchImageLibrary(
+      {
+        mediaType: 'photo',
+        maxHeight: 512,
+        maxWidth: 512,
+        includeBase64: Platform.OS === 'android',
+      },
+      resp => {
+        if (resp.didCancel) {
+          return;
+        }
+        setImage(resp);
+      },
+    );
+  };
   return (
     <View style={style.container}>
-      <View style={style.circle} />
+      <Pressable onPress={onSelectImage}>
+        <Image
+          style={style.circle}
+          source={
+            image
+              ? {uri: image?.assets[0]?.uri}
+              : require('../assets/icons/kbb/KBB3.png')
+          }
+        />
+      </Pressable>
+
       <View style={style.form}>
         <Input
           placeholder="닉네임"
@@ -40,11 +95,14 @@ export default function SetupProfile() {
           onSubmitEditing={onSubmit}
           returnKeyType="next"
         />
-
-        <View style={style.buttons}>
-          <Button title="다음" onPress={onSubmit} hasMarginBottom />
-          <Button title="취소" onPress={onCancel} />
-        </View>
+        {loading ? (
+          <ActivityIndicator size={32} color="#6200ee" style={style.spinner} />
+        ) : (
+          <View style={style.buttons}>
+            <Button title="다음" onPress={onSubmit} hasMarginBottom />
+            <Button title="취소" onPress={onCancel} />
+          </View>
+        )}
       </View>
     </View>
   );
@@ -69,5 +127,11 @@ const style = StyleSheet.create({
   },
   buttons: {
     marginTop: 48,
+  },
+  spinner: {
+    alignItems: 'center',
+    marginTop: 24,
+    paddingHorizontal: 16,
+    width: '100%',
   },
 });
--- kbb_rn_app/screens/ScheduleScreen.js
+++ kbb_rn_app/screens/ScheduleScreen.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import useUserContext from '../hooks/useUserContext';
 
 function ScheduleScreen({navigation}) {
+  const {user, setUser} = useUserContext();
+
   return (
     <View>
+      {user.photoURL && (
+        <Image
+          source={{uri: user.photoURL}}
+          style={style.photo}
+          resizeMode="cover"
+        />
+      )}
       <Text>ScheduleScreen</Text>
     </View>
   );
 }
+const style = StyleSheet.create({
+  photo: {
+    width: 128,
+    height: 128,
+    marginBottom: 16,
+    borderRadius: 64,
+  },
+});
 export default ScheduleScreen;
